cd2452846d78f5f05e576849dd26cccc72b616a8
JoanChirinos <jchirinos@stuy.edu>
2018-10-04 18:23:30 -0400

    finished hw

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -16,19 +16,31 @@ c = db.cursor()               #facilitate db ops
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 
 with open('peeps.csv', newline='\n') as peeps:
+    #Turns peeps.csv into a DictReader with first row as keys
     peepDictReader = csv.DictReader(peeps)
+
+    #Creates TABLE peeps with name, age, and id
     c.execute("CREATE TABLE peeps(name TEXT, age INTEGER, id INTEGER PRIMARY KEY);")
+
+    #Takes each peep and inputs respective vals into TABLE peeps
     for each in peepDictReader:
         command = "INSERT INTO peeps VALUES('" + each['name'] + "', " + each['age'] + ", " + each['id'] + ");"#build SQL stmt, save as string\
-        print(command)
-        c.execute(command)    #run SQL statement
+        #print(command)
+        #executes SQLite statement
+        c.execute(command)
 
 with open('courses.csv', newline='\n') as courses:
+    #Turns courses.csv into a DictReader with first row as keys
     courseDictReader = csv.DictReader(courses)
-    c.execute("CREATE TABLE courses(name TEXT, mark INTEGER, id INTEGER PRIMARY KEY);")
+
+    #Creates TABLE courses with name, mark, and id
+    c.execute("CREATE TABLE courses(name TEXT, mark INTEGER, id INTEGER);")
+
+    #Takes each course and inputs respective vals into TABLE peeps
     for each in courseDictReader:
-        command = "INSERT INTO peeps VALUES('" + each['name'] + "', " + each['age'] + ", " + each['id'] + ");"#build SQL stmt, save as string\
-        print(command)
+        command = "INSERT INTO courses VALUES('" + each['code'] + "', " + each['mark'] + ", " + each['id'] + ");"#build SQL stmt, save as string\
+        #print(command)
+        #executes SQLite statement
         c.execute(command)    #run SQL statement
 
 #==========================================================
