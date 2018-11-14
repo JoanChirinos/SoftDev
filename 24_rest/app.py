67c2926f639349b57501b2617a3398f566ac787c
JoanChirinos <jchirinos@stuy.edu>
2018-11-13 20:43:12 -0500

    did hw

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -4,21 +4,31 @@
 #2018-11-13
 
 from flask import Flask, render_template, request, session, url_for, redirect
-import urllib.request, ssl
+import urllib.request, json
 app = Flask(__name__) #create instance of class Flask
 
 @app.route('/')
 def root():
     url = 'https://api.nasa.gov/planetary/apod?api_key=x9CuMbqcX0mo5j5BF8pQGYaTx7R7dGSqSsOUQpmk'
     json_text = urllib.request.urlopen(url)
-    print('\n\n\n\n\n\n\n\n\nOpened url\n\n\n\n\n\n\n\n\n')
-    json_dict = json.loads(json_text)
+    json_dict = json.loads(json_text.read())
 
-    title = json_dict['title']
-    content = json_dict['content']
-    img_url = json_dict['url']
+    # print('DICT:\n{}'.format(json_dict))
 
-    return render_template('base.html', title=title, content=content, img_url=img_url)
+    title = json_dict.get('title', 'TITLE')
+    content = json_dict.get('explanation', 'NO EXPLANATION')
+    media_type = json_dict.get('media_type', 'NO TYPE')
+    media_url = json_dict.get('url', 'NO URL')
+    media = ''
+
+    if (media_type == 'video'):
+        media = '<iframe src={}></iframe>'.format(media_url)
+    elif (media_type == 'image'):
+        media = '<img src={} style="max-height: 60vh; max-width: 80vw;">'.format(media_url)
+    else:
+        media = '<p>NO MEDIA</p>'
+
+    return render_template('base.html', title=title, content=content, media=media)
 
 
 if __name__ == '__main__':
